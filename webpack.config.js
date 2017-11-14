2067ce5434704a384bd7f87de1e9251e54f60c01
Sikender Khan <sikendermkhan@gmail.com>
2017-11-14 21:24:01 +0530

    Initial project setup

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/app.jsx',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.jsx?/,
+      exclude: /node_modules/
+    }]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
+}
